120d15e2c6bf7865e8267c4be5a922db992768ad
Ingo Steinke <ingosteinke@ingosteinke.de>
2026-08-15 21:40:08 +0200

    astro typescript setup wip

---
+++ astro.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// import type { PluginOption } from 'vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tsconfigPaths() as any],
+    // Enable detailed error reporting
+    // mode: 'development',
+    // build: {
+    //  minify: false,
+    //  sourcemap: true
+    // }
+  },
+  site: 'https://thetextilebuffet.com',
+  base: '/',
+  trailingSlash: 'always',
+  // additional debug info:
+  // devToolbar: {
+  //  enabled: true
+  // }
+  redirects: {
+  },
+});
